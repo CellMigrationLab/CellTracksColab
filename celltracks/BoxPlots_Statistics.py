f186f0029510741b9ce335ff5b7355e60876f73c
guijacquemet <guillaume.jacquemet@utu.fi>
2025-02-27 18:49:50 +0200

    Update BoxPlots_Statistics.py

--- celltracks/BoxPlots_Statistics.py
+++ celltracks/BoxPlots_Statistics.py
@@ -23,8 +23,7 @@ from sklearn.preprocessing import MinMaxScaler
 computed_metrics = {
     'Track Metrics': [
         'Track Duration', 'Mean Speed', 'Median Speed', 'Max Speed', 'Min Speed',
-        'Speed Standard Deviation', 'Directionality', 'Total Distance Traveled', 'Spatial Coverage', 'Tortuosity', 'Total Turning Angle'
-    ],
+        'Speed Standard Deviation', 'Directionality', 'Total Distance Traveled', 'Spatial Coverage', 'Tortuosity', 'Total Turning Angle', 'FMI_x_plus','FMI_x_minus', 'FMI_y_plus','FMI_y_minus'],
     'Rolling Track Metrics': [
         'Mean Speed Rolling', 'Median Speed Rolling', 'Max Speed Rolling',
         'Min Speed Rolling', 'Speed Standard Deviation Rolling',
